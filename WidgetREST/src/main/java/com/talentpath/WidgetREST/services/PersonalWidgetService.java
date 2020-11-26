20351afe91f1639cda50b7cc4890a0bc41d0ddb3
dsmelser <dsmelser.personal@gmail.com>
2020-11-25 18:14:28 -0600

    cleaned up some comments and debugging code

--- WidgetREST/src/main/java/com/talentpath/WidgetREST/services/PersonalWidgetService.java
+++ WidgetREST/src/main/java/com/talentpath/WidgetREST/services/PersonalWidgetService.java
@@ -24,29 +24,11 @@ public class PersonalWidgetService {
         User currentUser = userRepo.findByUsername(name).get();
 
 
-        currentUser.getRoles().stream().forEach( r-> System.out.println(r.getName()) );
-        //not working?!??????????
-        //it's an enum not a string, stupid...
 
         boolean isAdmin = currentUser.getRoles()
                 .stream()
                 .anyMatch(
                     roleToCheck -> roleToCheck.getName() == Role.RoleName.ROLE_ADMIN);
-
-        //boolean isAdmin = currentUser.getRoles().stream().filter( r->r.getName().equals("ROLE_ADMIN") ).findAny().isPresent();
-
-       // boolean isAdmin = false;
-
-//        Role[] allRoles = currentUser.getRoles().toArray( new Role[currentUser.getRoles().size()]);
-//
-//        for( int i = 0; i < allRoles.length; i++ ){
-//            if( allRoles[i].)
-//        }
-
-
-
-
-
 
         if( isAdmin ){
             return repo.findAll();
